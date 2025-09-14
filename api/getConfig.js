21254ac1613b6d96794c630e9c73489f3a6b9c27
SirSluginston <Sirsluginston@gmail.com>
2025-09-14 12:04:34 -0400

    Update Lambda function for AWS SDK v3

--- api/getConfig.js
+++ api/getConfig.js
@@ -1,8 +1,9 @@
 // Lambda function: getConfig.js
 // This function fetches project/page config from DynamoDB
 
-const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb");
+const client = new DynamoDBClient();
 
 // Environment variables: TABLE_NAME
 exports.handler = async (event) => {
@@ -16,26 +17,33 @@ exports.handler = async (event) => {
         };
     }
 
+
     const params = {
         TableName: process.env.TABLE_NAME,
         Key: {
-            ProjectKey: project,
-            PageKey: page
+            ProjectKey: { S: project },
+            PageKey: { S: page }
         }
     };
 
     try {
-        const result = await dynamo.get(params).promise();
+        const command = new GetItemCommand(params);
+        const result = await client.send(command);
         if (!result.Item) {
             return {
                 statusCode: 404,
                 body: JSON.stringify({ error: 'Config not found' })
             };
         }
+        // Convert DynamoDB item to plain JS object
+        const unmarshalled = {};
+        for (const key in result.Item) {
+            unmarshalled[key] = Object.values(result.Item[key])[0];
+        }
         return {
             statusCode: 200,
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(result.Item)
+            body: JSON.stringify(unmarshalled)
         };
     } catch (err) {
         return {
